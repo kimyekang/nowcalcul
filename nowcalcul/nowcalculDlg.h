a6ef632cff877717a885a5951abaaac4ae637d4e
kimyekang <gongg10@naver.com>
2021-04-29 02:06:29 +0900

    초기 커밋.

---
+++ nowcalcul/nowcalculDlg.h
@@ -0,0 +1,51 @@
+﻿
+// nowcalculDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CnowcalculDlg 대화 상자
+class CnowcalculDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CnowcalculDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_NOWCALCUL_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnClickedButt0();
+	afx_msg void OnClickedButt1();
+	afx_msg void OnClickedButt2();
+	afx_msg void OnClickedButt3();
+	afx_msg void OnClickedButt4();
+	afx_msg void OnClickedButt5();
+	afx_msg void OnClickedButt6();
+	afx_msg void OnClickedButt7();
+	afx_msg void OnClickedButt8();
+	afx_msg void OnClickedButt9();
+//	CString m_op1;
+	double m_op1;
+	double m_op2;
+//	CEdit m_LCD;
+	double m_LCD;
+	CEdit m_Lcd;
+};
---
+++ nowcalcul/resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// nowcalcul.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_NOWCALCUL_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTT_1                      1000
+#define IDC_BUTT_2                      1001
+#define IDC_BUTT_3                      1002
+#define IDC_BUTT_4                      1003
+#define IDC_BUTT_5                      1004
+#define IDC_BUTT_6                      1005
+#define IDC_BUTT_7                      1006
+#define IDC_BUTT_8                      1007
+#define IDC_BUTT_9                      1008
+#define IDC_BUTT_0                      1009
+#define IDC_EDIT_op1                    1010
+#define IDC_EDIT2                       1011
+#define IDC_EDIT_LCD                    1011
+#define IDC_EDIT_op2                    1012
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
